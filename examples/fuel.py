93a300e4e368e1bf9e4f3b8782a7a614e6484998
Adam McDaniel <kiwi@Adams-MacBook-Pro.local>
2019-01-03 02:39:42 -0500

    Removed duplicate smooth in readme

--- examples/fuel.py
+++ examples/fuel.py
@@ -4,6 +4,8 @@ from small_vision import *
 image = cv2.imread("fuel.png", 1)
 
 
+# show untouched image
+# convert to HSV
 original = Image(image).show("original").convert_to_hsv()
 
 # HSV mask for neon yellow
@@ -12,7 +14,13 @@ mask = original.get_mask(
     [40, 255, 255]
 )
 
-original.mask(mask).smooth().blur(1).draw_target(mask).convert_to_bgr().show("output")
+# apply mask
+# blur 100%
+# draw circle on the largest blob
+# convert to HSV
+# convert back to BGR
+# show image
+original.mask(mask).blur(1).draw_target(mask).convert_to_bgr().show("output")
 
 
 wait_for_keypress()
